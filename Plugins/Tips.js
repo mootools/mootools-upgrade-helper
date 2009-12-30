acedb51b3e63806b5490ae78d958c7a970e3c3c4
Aaron Newton <git@clientcide.com>
2009-12-29 16:18:34 -0800

    adding backwards compat for tips for css styles adding warnings.

--- Plugins/Tips.js
+++ Plugins/Tips.js
@@ -3,10 +3,19 @@
 	Tips.implement({
 
 		initialize: function(){
+			MooCompat.log('1.1 > 1.2: Tips DOM element layout has changed and your CSS classes may need to change.');
 			var params = Array.link(arguments, {options: Object.type, elements: $defined});
 			this.setOptions(params.options);
-			if (this.options.offsets) this.options.offset = this.options.offsets;
+			if (this.options.offsets) {
+				MooCompat.log('1.1 > 1.2: Tips no longer have an "offsets" option; use "offset".');
+				this.options.offset = this.options.offsets;
+			}
 			document.id(this);
+			this.addEvent('show', function(){
+				this.tip.addClass('tool-tip');
+				this.tip.getElement('.tip-title').addClass('tool-title');
+				this.tip.getElement('.tip-text').addClass('tool-text');
+			});
 			this.parseTitle(params.elements);
 			if (params.elements) this.attach(params.elements);
 		},
@@ -15,6 +24,7 @@
 			elements.each(function(element){
 			var title = element.get('title');
 				if (title.test('::')) {
+					MooCompat.log('1.1 > 1.2: Tips no longer parse the title attribute for "::" for title/caption; use title and rel attributes instead.');
 					element.store('tip:title', title.split('::')[0]);
 					element.store('tip:text', title.split('::')[1]);
 					element.set('title', '');
