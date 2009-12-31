46797df475bb289fa2eee7ceb8db31815d75c811
Aaron Newton <git@clientcide.com>
2009-12-31 11:53:22 -0800

    adding some semi-colons

--- Utilities/JSON.js
+++ Utilities/JSON.js
@@ -1,10 +1,10 @@
 JSON.toString = function(obj){ 
 	MooTools.upgradeLog('1.1 > 1.2: JSON.toString is deprecated. Use JSON.encode');
 	return JSON.encode(obj); 
-}
+};
 JSON.evaluate = function(str){
 	MooTools.upgradeLog('1.1 > 1.2: JSON.evaluate is deprecated. Use JSON.decode');
 	return JSON.decode(str); 
-}
+};
 var Json = JSON;
 
